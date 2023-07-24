da770d77f5a991023f8bbd2af0251672d67051a9
yangseunghun <simononze@naver.com>
2023-07-24 23:55:15 +0900

    [스프링 DB 접근 기술2]테스트 - 각 테스트의 독립적 실행을 위한 데이터 롤백 적용

--- itemservice-db-start/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db-start/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,26 +5,57 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+/**
+ * @SpringBootTest 는 @SpringBootApplication 을 찾아서 설정으로 사용함
+ * @SpringBootApplication 설정이 MemoryConfig.class 였다 JdbcTemplateV3Config.class 로 변경됐기 때문에 JdbcTemplate 을 통해 실제 디비를 호출함
+ */
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
+    /**
+     * 트랜잭션 관리자 주입
+     * 스프링 부트는 적절한 트랜잭션 매니저 빈을 스프링 빈으로 등록해줌
+     */
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    TransactionStatus status;
+
+    /**
+     * 갹각의 테스트 케이스를 실행하기 직전에 호출되기 때문에 여기서 트랜잭션을 시작하면 됨
+     */
+    @BeforeEach
+    void beforeEach(){
+        //트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    /**
+     * 각각의 테스트 케이스가 완료된 직후에 호출되기 때문에 여기서 트랜잭션을 롤백하면 됨
+     */
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        //트랜잭션 롤백
+        transactionManager.rollback(status);
     }
 
     @Test
